772cf62f1aefcc0ea5d59a8b5f8295c9b3a1ce08
Paul Vidart <paulvidart@gmail.com>
2022-05-24 17:34:33 -0300

    Change style alert dialog

--- app/src/main/java/com/example/memorygame/components/AlertDialogSample.kt
+++ app/src/main/java/com/example/memorygame/components/AlertDialogSample.kt
@@ -1,14 +1,17 @@
 package com.example.memorygame.components
 
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.memorygame.R
 import com.example.memorygame.model.Card
@@ -29,11 +32,18 @@ fun AlertDialogSample(
             openDialog.value = false
         },
         title = {
-            Text(
-                text =
-                if (time.value == 0) "Time's up! Try again!"
-                else "Congratulations, you got them all right!!!!"
-            )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text =
+                        if (time.value == 0) "Time's up! Try again!"
+                        else "Congratulations, you got them all right!!!!"
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
+            }
+
         },
         dismissButton = {
             TextButton(
